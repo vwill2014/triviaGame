ab9619275d0d3d522e2bfebc7b050d6982f01d57
Veronica Williams <vcwilliams2014@gmail.com>
2017-11-02 23:57:52 -0700

    Updating All Files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-// 
-
 // For fun, set a timer on the pictures of Durant so they rotate through a series of pictures as a person plays the game.
 
 	$(function(){
@@ -7,6 +5,7 @@
 	var images = ["assets/images/kevin_durant.jpg", "assets/images/kevin5.jpg", "assets/images/kevin3.jpg", "assets/images/kevin2.jpg","assets/images/kevin6.jpg"];
 	var showImage;
 	var count = 0;
+	
 
 	$("#start").click(startSlideShow);
 
@@ -16,7 +15,7 @@
 // to do this by changing the src attribute to point to the image[count]. This is 
 // where my images live.
 
-	$("#kevinCol").attr("src", images[count]);
+$("#kevinCol").attr("src", images[count]);
 
 console.log(images[count]);
 	}
@@ -36,7 +35,7 @@ displayImage()
 }
   function startSlideShow(){
 
-  	showImage = setInterval(nextImage, 5000);
+  	showImage = setInterval(nextImage, 10000);
 
   }
 
@@ -49,7 +48,7 @@ displayImage()
   displayImage();
 });
 
-	
+
  // trivia question code========================================
 
 // This Trivia quizz is based on True or False questions.
@@ -57,86 +56,146 @@ displayImage()
 // 2. create 5 questions with an array of question and correct answer that displays randomly.
 // 2. For each question create a button for false and button for true.
 // 5. Create a rule that states when answer === false html displays "Loser"
-// 6. Create a rule that states when answer === true display displayed in the array for each question
+// 6. Create a rule that states when answer === true html displays "You're Write"
 // 
-// // 
-// 	var questionAnswer =[
-// 	{"question":"Durant begain wearing #35 as his jersey number in honor of his AAU coach Charles Craig, who was murdered at the age of 35.",
-// 	"answer": "true"},
+// // 	
+// var qCounter = 0;
+// var answerCounter = 1;
 
-// 	{"question":"In the 2007 NBA draft, Durant was the first overall pick."
-// 	"answer" : "false"
-// 	},
+// 	var questionAnswer = {
+// 	 	q1:"Durant begain wearing #35 as his jersey number in honor of his AAU coach Charles Craig, who was murdered at the age of 35.",
+// 		a1:"true",
+// 		q2:"In the 2007 NBA draft, Durant was the first overall pick.",
+// 		a2:"false",
+// 		q3: "Durant played 9 seasons with the Oklahoma Thunders.",
+// 		a3:"true",
+// 		q4: "Durant describes himself as a 'high shcool kid.'",
+// 		a4: "true",
+// 		q5: "Durant’s jersey seldom ranks as one of the NBA’s best-selling.",
+// 		a5: "false"
+// 	};
 
-// 	{"question":"Durant played 9 seasons with the Oklahoma Thunders.",
-// 	"answer"= "true"},
+// 	$("#start").(function(){
+// 	$("#questions").text(questionAnswer.q2);
 
-// 	{"question":"Durant describes himself as a 'high shcool kid.' ",
-// 	"answer"= "true"},
+// 		// questionAnswer[1] = ,"false";
+	
+// 		// questionAnswer [2] = ".", "true";
 
-// 	{"question":"Durant’s jersey seldom ranks as one of the NBA’s best-selling and he is consistently one of the top All-Star vote-getters.",
-// 	"answer"= "false"},
-// ]
+// 		// questionsAnswer [3] = , "true";
+	
+// 		// questionAnswer [4] = , "false";
+
+// });	
+// 	// S
+// 	// 	else if (questionAnswer.a1 === "false"){
 
 
+// 	// 		$("#false").attr("type", "Great Job!!!");
 
+// 	// 	}
 
+// 	// };
+	
+// // $("#start").click(function(){
 
+// // $("#questions").text(questionAnswer.q1)
+// //  // qCounter++;
+// // });
 
-// 	var selection = content[Math.floor(Math.random()questionAnswer.length)]''
-// 	questions[2] = [];
-// 	questions[3] = ["Durant played 9 seasons with the Oklahoma Thunders."];
-// 	questions[4] = ["Durant describes himself as a 'high shcool kid'."];
 	
 
+// // write a function called check answer outside of my click.
+// // If statement within my check answer function
+// // $(id).text() this will reseach this
+		
 	
-// 	// var randomQuestions = Math.floor(Math.random()[questions.length] + 1);
-// 	// console.log(randomQuestions);
-
-// $("#start").click(function(){
-
-// $("#questions").html(questions[2]);
-
-// 	// for(var i = 0; i<questions.length; i++){
-
-// 	});
-// // I need to make the below code generate the questions above randomly.
-// // Where I currently have questions[3]is where I need to put the Math.floor(Math.random) for my array
-// // I am having trouble with the syntax of doing that
+	
+// // 	});
+// // // I need to make the below code generate the questions above randomly.
+// // // Where I currently have questions[3]is where I need to put the Math.floor(Math.random) for my array
+// // // I am having trouble with the syntax of doing that
 
 
-// // $("#questions").html(Math.floor(randomQuestion);
+// // // $("#questions").html(Math.floor(randomQuestion);
 
-// });
-
-
+// // });
 
 
-
-
-
-
-
-// $("#questions").html( = "question " + (position+1) + "  of" + " 10";
-//     question = questions[position] [0];
-//     chA =questions[position] [1];
-//     chB =questions[position] [2];
-//     chC=questions[position] [3];
-//     chD = questions[position] [4];
-//     test.innerHTML = "<h3>" + question + "</h3>"; 
-//     test.innerHTML +="<input type='radio' id='choices' value='A'> "+chA+"<br /> ";
-//     test.innerHTML +="<input type='radio' id='choices' value='B'> "+chB+"<br /> ";
-//     test.innerHTML +="<input type='radio' id='choices' value='C'> "+chC+"<br /> ";
-//     test.innerHTML +="<input type='radio' id='choices' value='D'> "+chD+"<br /> ";
-//     test.innerHTML += "<button onclick='checkAnswer ()'> Submit Answer</button>)";)
-
-//     }
-// function checkAnswer() {
-//     choices = document.getElementById("choices");
-//     for (var i = 0; i < choices.length; i++){
-//         if (choices [i].checked){
-//             choices = choices [i].value; 
-//     }
 // }
-// if (choice === questions[position][5]){
-//      correct ++;
+	
+	
+	var questions = ["In the 2007 NBA draft, Durant was the first overall pick.", "Durant played 9 seasons with the Oklahoma Thunders.", "Durant begain wearing #35 as his jersey number in honor of his AAU coach Charles Craig, who was murdered at the age of 35.", "Durant’s jersey seldom ranks as one of the NBA’s best-selling.","Durant describes himself as a 'high shcool kid.'"];
+	var showQuestions;
+	var countTwo = 0;
+
+	$(this).click(displayQuestions);
+	
+	function displayQuestions(){
+
+//this function allows me to replace the images within my array. It allows me to 
+// to do this by changing the src attribute to point to the image[count]. This is 
+// where my images live.
+
+	$("#questions").text(questions[countTwo]);
+console.log (questions[countTwo]);
+	}
+	
+	function nextQuestions (){
+
+	countTwo++;
+
+	if (countTwo === questions.length){
+
+		countTwo = 0;
+	
+	}
+
+displayQuestions()
+
+}
+  function startSlideShowTwo(){
+
+  	showQuestions = setInterval(nextQuestions, 10000);
+
+  }
+
+  function stopSlideShowTWo(){
+
+  	clearInterval(showQuestions);
+
+  }
+
+  displayQuestions();
+
+// var counter = {
+
+
+
+
+// 	}
+// 	setTimeout(tenSeconds, 1000 * 10);
+// 	function tenSeconds(){ 
+
+// 		$("#timer").text("00:00")
+
+
+// 	}
+	
+// 	timeConverter: function(t) {
+
+//     var seconds = t - (minutes * 10);
+
+//     if (seconds < 10) {
+//       seconds = "0" + seconds;
+//     }
+
+//     if (minutes === 0) {
+//       minutes = "00";
+//     }
+//     else if (minutes < 10) {
+//       minutes = "0" + minutes;
+//     }
+
+//     return minutes + ":" + seconds;
+//   }
